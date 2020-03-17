f9729284a8c8ac0c7e85f857d8146bc1a7ae3b07
moreiradev <moreiradev@hotmail.com>
2020-03-17 17:41:03 +0000

    Updating exercises

--- TP4/main.cpp
+++ TP4/main.cpp
@@ -107,24 +107,14 @@ void bubbleSort(vector<int> &v, char op){
 
 
 bool ascending(int x, int y){
-    if (x > y){
-        return true;
-    }
-    else{
-        return false;
-    }
+    return x > y;
 }
 bool descending(int x, int y){
-    if (x < y){
-        return true;
-    }
-    else{
-        return false;
-    }
+    return x < y;
 }
 
 
-void bubbleSort(vector<int> &v, bool f(int x, int y)){
+vector<int> bubbleSort(vector<int> v, bool f(int x, int y)){
     bool done = false;
     int iter = 1;
     while (!done || iter != v.size()){
@@ -139,16 +129,124 @@ void bubbleSort(vector<int> &v, bool f(int x, int y)){
         }
         iter++;
     }
+    return v;
+}
+
+
+// EXERCISE 4
+
+int binarySearch(const vector<int> v, int value){
+    int low = 0, high = v.size();
+    bool found = false;
+
+    while(low < high){
+        int mid = (low + high)/2;
+
+        if (v[mid] == value){
+            return mid;
+        }
+        else if (v[mid] > value){
+            if (mid == high){
+                break;
+            }
+            high = mid;
+        }
+        else if (v[mid] < value) {
+            if (mid == low){
+                break;
+            }
+            low = mid;
+        }
+    }
+    return -1;
+}
+
+
+// EXERCISE 5
+
+
+
+void removeDuplicates(vector<int> &v){
+    int idx = 0;
+    while(idx < v.size()){
+        int currVal = v[idx];
+        // Search for duplicates
+        int findIdx = idx + 1;
+        while (findIdx < v.size()){
+            if (v[findIdx] == currVal){
+                int replaceIdx = findIdx+1;
+                while (replaceIdx < v.size()){
+                    int temp = v[replaceIdx];
+                    v[replaceIdx-1] = v[replaceIdx];
+                    replaceIdx++;
+                }
+                v.resize(v.size()-1);
+            }
+            findIdx++;
+        }
+        idx++;
+    }
 }
 
 
 
+// EXERCISE 6
 
+
+void vectorUnion(const vector<int> v1, const vector<int> v2, vector<int> &v3){
+    for (int i = 0; i < v1.size(); ++i) {
+        v3.push_back(v1[i]);
+    }
+    for (int i = 0; i < v2.size(); ++i) {
+        v3.push_back(v2[i]);
+    }
+
+    removeDuplicates(v3);
+    v3 = bubbleSort(v3, ascending);
+}
+
+
+void vectorIntersection(const vector<int> v1, const vector<int> v2, vector<int> &v3){
+    for (int i = 0; i < v1.size(); ++i) {
+        if (binarySearch(bubbleSort(v2, ascending), v1[i]) != -1){
+            v3.push_back(v1[i]);
+        }
+    }
+    v3 = bubbleSort(v3, ascending);
+    removeDuplicates(v3);
+}
+
+
+
+// EXERCISE 7
+
+
+void localMax(int NE, const int a[][NE]){
+    for (int i = 1; i < NE-1; ++i) {
+        for (int j = 1; j < NE-1; ++j) {
+            int currVal = a[i][j];
+            bool valid = true;
+            for (int si = -1; i < 2; ++i) {
+                for (int sj = -1; j < 2; ++j) {
+                    if(a[si+i][sj+j] > currVal){
+                        valid = false;
+                    }
+                }
+            }
+
+            if (valid)
+                cout << currVal << " ";
+
+        }
+    }
+}
 
 int main() {
-    vector<int> v = {50, 20, 1, 5, 10, 10, 2, 10};
-    bubbleSort(v, ascending);
-    for (int i = 0; i < v.size(); ++i) {
-        cout << v[i] << " ";
+    vector<int> v = {20, 1, 5, 10, 10, 50, 10};
+    vector<int> v2 = {50,20,3,15,50};
+    vector<int> v3;
+    vectorUnion(v, v2,v3);
+    for (int i = 0; i < v3.size(); ++i) {
+        cout << v3[i] << " ";
     }
 }
