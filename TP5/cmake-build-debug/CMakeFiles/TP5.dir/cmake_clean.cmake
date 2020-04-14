e6b1c73cf8e75e8010ca2199da9e332a6e2ae25e
moreiradev <moreiradev@hotmail.com>
2020-04-14 10:37:59 +0100

    Added some exercises from exercise 5.

---
+++ TP5/cmake-build-debug/CMakeFiles/TP5.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TP5.dir/main.cpp.o"
+  "TP5"
+  "TP5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TP5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
